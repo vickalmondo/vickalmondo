47a134beac9eac2911a1cbd797fd82c57e3d2a6c
[dyad] <git@dyad.sh>
2026-02-21 13:06:35 +0100

    [dyad] Disabling component tagging to fix R3F errors - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
@@ -9,9 +8,6 @@ export default defineConfig(() => ({
     port: 8080,
   },
   plugins: [
-    dyadComponentTagger({
-      exclude: [/Showcase.tsx/]
-    }), 
     react()
   ],
   resolve: {
